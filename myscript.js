16349cf3d430587285faa5908d5b1df5e758825b
Valentin-1981 <chibinyov@rambler.ru>
2024-12-04 10:11:46 +0300

    nineth commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+$(".cross").on("click", function () {
+    // $(".drop_down").css("visibility", "hidden");
+    $(".drop_down").css("display", "none");
+});
+
+$(".upper_header__img1").on("click", function () {
+    $(".drop_down").css("display", "block");
+});
